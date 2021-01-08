e8304733f6236a2df4ab23274025fb4e3f286b8f
NeMoMo <piotr@theworlt.com>
2021-01-08 22:37:36 +0100

    61. Spring Pet Clinic - Refactor Services to Common Interface

--- pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/services/PetService.java
@@ -2,13 +2,5 @@ package momo.springframework.sfgpetclinic.services;
 
 import momo.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/services/VetService.java
@@ -2,13 +2,5 @@ package momo.springframework.sfgpetclinic.services;
 
 import momo.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
