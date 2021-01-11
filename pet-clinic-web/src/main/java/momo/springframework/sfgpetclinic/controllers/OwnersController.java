0afaabd2feed1dba91b3674a0114de0bee50feae
NeMoMo <piotr@theworlt.com>
2021-01-11 22:36:55 +0100

    82/83 List owners and vets

--- pet-clinic-web/src/main/java/momo/springframework/sfgpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/momo/springframework/sfgpetclinic/controllers/OwnersController.java
@@ -1,14 +1,24 @@
 package momo.springframework.sfgpetclinic.controllers;
 
+import momo.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("owners")
 @Controller
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping("list")
-    public String list() {
+    public String list(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/list";
     }
 }
--- pet-clinic-web/src/main/java/momo/springframework/sfgpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/momo/springframework/sfgpetclinic/controllers/VetsController.java
@@ -1,14 +1,24 @@
 package momo.springframework.sfgpetclinic.controllers;
 
+import momo.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("vets")
 @Controller
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping("list")
-    public String list() {
+    public String list(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/list";
     }
 }
