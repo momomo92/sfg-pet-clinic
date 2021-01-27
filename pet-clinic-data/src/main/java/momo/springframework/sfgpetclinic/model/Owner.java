6de069aab4e548578a66684048a41df5be127d08
NeMoMo <piotr@theworlt.com>
2021-01-27 23:08:27 +0100

    120. Spring Pet Clinic - Add Contact Info to Owner

--- pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/model/Owner.java
@@ -3,7 +3,27 @@ package momo.springframework.sfgpetclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+
+    private String address;
+    private String city;
     private Set<Pet> pets;
+    private String telephone;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 
     public Set<Pet> getPets() {
         return pets;
@@ -11,5 +31,13 @@ public class Owner extends Person {
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
--- pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/model/Visit.java
@@ -3,6 +3,7 @@ package momo.springframework.sfgpetclinic.model;
 import java.time.LocalDate;
 
 public class Visit extends BaseEntity {
+
     private LocalDate date;
     private String description;
     private Pet pet;
