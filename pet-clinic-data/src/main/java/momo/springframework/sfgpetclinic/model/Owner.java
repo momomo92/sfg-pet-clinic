436832305f7f7cd72e7ec39ccf014a3def0fb97a
NeMoMo <piotr@theworlt.com>
2021-02-03 21:48:00 +0100

    146. Spring Pet Clinic - Convert Owners to JPA Entities

--- pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/model/Owner.java
@@ -1,13 +1,25 @@
 package momo.springframework.sfgpetclinic.model;
 
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
+
+    @Column(name = "telephone")
     private String telephone;
 
     public String getAddress() {
--- pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/model/Pet.java
@@ -1,12 +1,24 @@
 package momo.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     public LocalDate getBirthDate() {
