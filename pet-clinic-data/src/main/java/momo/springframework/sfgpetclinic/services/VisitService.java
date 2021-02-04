62ed0f7c6ee9d95094d5b8ff86742af1d6ec7cc4
NeMoMo <piotr@theworlt.com>
2021-02-04 21:54:49 +0100

    156. Spring Pet Clinic - Add Map Based Visit Service

---
+++ pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package momo.springframework.sfgpetclinic.services;
+
+import momo.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,33 @@
+package momo.springframework.sfgpetclinic.services.map;
+
+import momo.springframework.sfgpetclinic.model.Visit;
+import momo.springframework.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
