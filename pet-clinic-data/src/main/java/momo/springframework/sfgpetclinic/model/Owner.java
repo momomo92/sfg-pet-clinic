cce0aecfaff9dc42d5de00e62241594d1ffe611f
NeMoMo <piotr@theworlt.com>
2021-02-05 20:25:42 +0100

    167. Spring Pet Clinic - Refactoring for Project Lombok

--- pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/momo/springframework/sfgpetclinic/model/Owner.java
@@ -1,11 +1,17 @@
 package momo.springframework.sfgpetclinic.model;
 
+import lombok.*;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
@@ -21,36 +27,4 @@ public class Owner extends Person {
 
     @Column(name = "telephone")
     private String telephone;
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
 }
